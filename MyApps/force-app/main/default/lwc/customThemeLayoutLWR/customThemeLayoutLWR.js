d18612849636b233d492cfa52a7eff0009fa864b
Guisselle <guisselle@kodebus.com>
2024-08-10 11:11:15 -0400

    created custom theme layout

---
+++ MyApps/force-app/main/default/lwc/customThemeLayoutLWR/customThemeLayoutLWR.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+/**
+ * @slot header - This is the header slot
+ * @slot footer - This is the footer slot
+ * @slot default - Default slot.
+ */
+
+export default class CustomThemeLayoutLWR extends LightningElement {
+
+
+}
